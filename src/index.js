7339fba831b29e5596b3a38ce5398217122346a7
yassin <58929848+yasin6606@users.noreply.github.com>
2020-08-15 16:37:45 +0430

    init react app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'w3-css/w3.css';
+import 'w3-css/4/w3pro.css';
+import '../node_modules/vazir-font/dist/font-face.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
