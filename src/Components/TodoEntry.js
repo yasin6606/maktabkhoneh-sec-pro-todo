56d5b1a369c6d7fbdbcb6d08ca390ef86de607f0
yassin <58929848+yasin6606@users.noreply.github.com>
2020-08-15 17:16:30 +0430

    change something in input class

--- src/Components/TodoEntry.js
+++ src/Components/TodoEntry.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdbreact';
+import './../CSS/Outline-none.css';
 
 class TodoEntry extends Component {
     render() {
         return (
             <>
-                <MDBContainer className="text-center">
+                <MDBContainer className="text-center col-6 py-2">
                     <MDBRow>
                         <MDBCol>
                             <MDBRow>
                                 <MDBCol>
                                     <h1>Todo App</h1>
+                                </MDBCol>
+                            </MDBRow>
+                            <MDBRow className="mt-3 border rounded-top shadow">
+                                <MDBCol className="py-2">
+                                    <input placeholder="What needs to be done ? " className="px-2 border-0 col-12 shadow-none" />
                                 </MDBCol>
                             </MDBRow>
                         </MDBCol>
